0758fcea96b6c32eff760d35a428f2a716086da3
richa <richardmel@outlook.com>
2019-03-31 17:56:24 -0500

    added a footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Apicall from './Apicall';
+import Apicall from './api/Apicall';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+    <div>
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Welcome to RKM API!</h1>
         </header>
 
-        <Apicall />
-      </div>
+        <div className="main container App">
+          <Apicall />
+        </div>
+
+        <Footer />
+    </div>
+
     );
   }
 }
