e52aefc73e93251a32e8d38cf671c2d8cd83c5b0
richa <richardmel@outlook.com>
2019-04-03 10:12:27 -0500

    final version

--- src/api/Apicall.js
+++ src/api/Apicall.js
@@ -8,7 +8,7 @@ class Apicall extends Component {
 
 
   componentWillMount(){
-    this.getReddit();
+    //this.getReddit();
   }
 
   getReddit(){
@@ -18,7 +18,7 @@ class Apicall extends Component {
       this.setState({posts});
     })
     .catch(error => {
-
+      console.log(error);
     });
   }
 
@@ -27,10 +27,38 @@ class Apicall extends Component {
     super(props);
 
     this.state = {
+      value: '',
       posts: [],
       subredt: 'technology'
     };
     this.getReddit = this.getReddit.bind(this);
+    this.getSearchResult = this.getSearchResult.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    // gets the user search topic
+    this.setState({value: event.target.value});
+  }
+
+  getSearchResult() {
+    if (this.state.value === '') {
+      // here we have no entered search topic
+      alert('Error, Please enter a search topic. ');
+
+    } else {
+      //alert('Entered search topic: ' + this.state.value.toLowerCase());
+      //var topic = this.state.value.toLowerCase();
+      axios.get(`https://www.reddit.com/r/${this.state.value.toLowerCase()}.json`)
+      .then(response => {
+        const posts = response.data.data.children.map(obj => obj.data);
+        this.setState({posts});
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+
   }
 
 
@@ -44,20 +72,22 @@ class Apicall extends Component {
 
         <div className="main container App">
 
-           <input id="icon_prefix" type="text" placeholder="Enter search Topic" class="validate"/>
+           <input id="icon_prefix" type="text" placeholder="Enter search Topic" class="validate" value={this.state.value} onChange={this.handleChange}/>
 
-           <button class="btn waves-effect waves-light" onClick={this.getReddit} >
+           <button class="btn waves-effect waves-light" onClick={this.getSearchResult} >
              Search
            </button>
 
 
-          <h1>{`/r/${this.state.subredt}`}</h1>
+          <h1>{`/r/${this.state.value.toLowerCase()}`}</h1>
           <ul>
             {this.state.posts.map(post =>
               <li key={post.id}>{post.title}</li>
             )}
           </ul>
         </div>
+
+        <div style={{height: '300px'}}>></div>
 
         <Footer />
 
